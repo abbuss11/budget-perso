f7e2f0b03978727d63d6b50f66cc948beab07081
abbuss <idrissabba14@gmail.com>
2026-02-06 10:12:17 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'budget-perso',
+  webDir: 'dist'
+};
+
+export default config;
