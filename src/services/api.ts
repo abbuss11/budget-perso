6e2eb50faef6425e7c2484d0afd04ebb7350f589
abbuss <idrissabba14@gmail.com>
2026-02-06 10:36:39 +0100

    pussh push

---
+++ src/services/api.ts
@@ -0,0 +1,9 @@
+// src/services/api.ts
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
